c42d2d6310ae43c6d3c4c3331ffac4af5d29e654
YuriiSmakota <yurii.smakota@gmail.com>
2023-12-27 20:34:46 +0200

    start_template

---
+++ bot.py
@@ -0,0 +1,24 @@
+import logging
+import asyncio
+
+from aiogram import Dispatcher, Bot
+from settings import config
+
+from heandlers import command_start
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+
+    bot = Bot(token=config.token.get_secret_value(), parse_mode="html")
+    dp = Dispatcher()
+
+    dp.include_router(command_start.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
